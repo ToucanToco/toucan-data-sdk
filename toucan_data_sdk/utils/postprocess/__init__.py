beaa1bd97292bfc5df736cbd8797b16d62730b96
vdestraitt <34442515+vdestraitt@users.noreply.github.com>
2018-03-08 10:21:37 +0100

    New util rank (#37)
    * Add rank util with test and modified __init__
    
    * Add rank util with test and modified __init__
    
    * Update rank.py

--- toucan_data_sdk/utils/postprocess/__init__.py
+++ toucan_data_sdk/utils/postprocess/__init__.py
@@ -7,6 +7,7 @@ from .melt import melt
 from .percentage import percentage
 from .pivot import pivot, pivot_by_group
 from .query_df import query, query_df
+from .rank import rank
 from .rename import rename
 from .replace import replace
 from .sort import sort_values
