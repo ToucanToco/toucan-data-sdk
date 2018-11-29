decf639e15e091d9d3501f5e9f3e102d4a2672dd
PrettyWood <eric.jolibois@toucantoco.com>
2018-11-29 23:45:08 +0100

    Feature :: drop duplicates postprocess (#89)

---
+++ tests/utils/postprocess/test_filter.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from toucan_data_sdk.utils.postprocess import drop_duplicates, query_df
+
+
+def test_query_df():
+    """ It should return a filtered data dict """
+    data = pd.DataFrame([{'value': 30}, {'value': 10}, {'value': 20}])
+    config = {'query': 'value > 15'}
+    filtered_data = pd.DataFrame([{'value': 30}, {'value': 20}], index=[0, 2])
+
+    df = query_df(data, **config)
+    assert df.equals(filtered_data)
+
+
+def test_drop_duplicates():
+    """It should drop duplicates"""
+    df = pd.DataFrame({'a': [1, 2, 3, 1], 'b': [0, 0, 1, 1], 'c': [3, 3, 2, 2], 'd': [0, 1, 1, 0]})
+    assert drop_duplicates(df, ['a']).shape == (3, 4)
+    assert drop_duplicates(df, ['b', 'c']).shape == (2, 4)
+    assert drop_duplicates(df, None).shape == (4, 4)
--- toucan_data_sdk/utils/postprocess/__init__.py
+++ toucan_data_sdk/utils/postprocess/__init__.py
@@ -2,12 +2,12 @@ from .argmax import argmax, argmin
 from .converter import convert_str_to_datetime, convert_datetime_to_str, cast, change_date_format
 from .cumsum import cumsum
 from .fillna import fillna
+from .filter import drop_duplicates, query, query_df
 from .groupby import groupby
 from .math import multiply, divide, add, subtract, formula
 from .melt import melt
 from .percentage import percentage
 from .pivot import pivot, pivot_by_group
-from .query_df import query, query_df
 from .rank import rank
 from .rename import rename
 from .replace import replace
