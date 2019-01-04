9e3d8edac8340efada348382b14be7a77c225a9d
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-01-04 13:58:06 +0100

    at start->END, START<-stop and minilanguage support
    - `start` only now means: from this date onwards,
    - `stop` only now means: until stil date (excluded),
    - `atdate` means: at this specific date
    
    and add support for offset syntax in date declaration, thus removing
    the need of a `delta` parameter: (e.g. : (mydate + 1d))

--- tests/utils/postprocess/test_filter_by_date.py
+++ tests/utils/postprocess/test_filter_by_date.py
@@ -5,6 +5,7 @@ from pandas.testing import assert_frame_equal
 import pytest
 
 from toucan_data_sdk.utils.postprocess import filter_by_date
+from toucan_data_sdk.utils.postprocess.filter_by_date import parse_date
 
 
 BEFORE_YESTERDAY = (date.today() - timedelta(days=2)).strftime('%Y-%m-%d')
@@ -43,21 +44,87 @@ def test_filter_date_invalid_calls(sample_data):
     """It should forbid invalid start/stop/delta combinations"""
     df = pd.DataFrame(sample_data)
     with pytest.raises(TypeError):
+        # no filter specification
         filter_by_date(df, 'date')
     with pytest.raises(TypeError):
-        filter_by_date(df, 'date', start='2018-01-01', stop='2018-01-01', delta='1d')
+        # start, stop and atdate can't be all specified
+        filter_by_date(df, 'date', start='2018-01-01', stop='2018-01-01', atdate='2018-01-01')
     with pytest.raises(TypeError):
-        filter_by_date(df, 'date', stop='2018-01-01')
+        # start and atdate are mutually exclusive
+        filter_by_date(df, 'date', start='2018-01-01', atdate='2018-01-01')
+    with pytest.raises(TypeError):
+        # stop and atdate are mutually exclusive
+        filter_by_date(df, 'date', stop='2018-01-01', atdate='2018-01-01')
     with pytest.raises(ValueError):
+        # bad date format
         filter_by_date(df, 'date', start='2018-01-01', date_fmt='%m %Y')
     with pytest.raises(ValueError):
+        # bad date format
         filter_by_date(df, 'date', start='01 2018', date_fmt='%m %Y')
+    with pytest.raises(ValueError):
+        # bad offset syntax (missing parenthesis)
+        filter_by_date(df, 'date', start='2018-01-01 + 1day', date_fmt='%m %Y')
+
+
+def test_filter_by_date_atdate(sample_data):
+    """It should filter rows on a specific date"""
+    df = pd.DataFrame(sample_data)
+    df = filter_by_date(df, 'date', atdate='2018-01-28')
+    expected = pd.DataFrame([
+        {'date': '2018-01-28', 'value': 0},
+        {'date': '2018-01-28', 'value': 1},
+    ])
+    assert_frame_equal_noindex(df, expected)
 
 
 def test_filter_by_date_start_only(sample_data):
-    """It should filter rows on a specific date"""
+    """It should filter rows after a specific date"""
     df = pd.DataFrame(sample_data)
-    df = filter_by_date(df, 'date', start='2018-01-28')
+    df = filter_by_date(df, 'date', start='2018-01-30')
+    expected = pd.DataFrame([
+        {'date': '2018-01-30', 'value': 3},
+        {'date': '2018-01-31', 'value': 4},
+        {'date': '2018-02-01', 'value': 5},
+        {'date': '2018-02-02', 'value': 6},
+        {'date': YESTERDAY, 'value': 7},
+        {'date': TODAY, 'value': 8},
+        {'date': TOMORROW, 'value': 9},
+        {'date': BEFORE_YESTERDAY, 'value': 10},
+    ])
+    assert_frame_equal_noindex(df, expected)
+
+
+def test_filter_by_date_start_only_with_offset(sample_data):
+    """It should filter rows after a specific date with offset"""
+    df = pd.DataFrame(sample_data)
+    df = filter_by_date(df, 'date', start='(2018-01-30) + 2days')
+    expected = pd.DataFrame([
+        {'date': '2018-02-01', 'value': 5},
+        {'date': '2018-02-02', 'value': 6},
+        {'date': YESTERDAY, 'value': 7},
+        {'date': TODAY, 'value': 8},
+        {'date': TOMORROW, 'value': 9},
+        {'date': BEFORE_YESTERDAY, 'value': 10},
+    ])
+    assert_frame_equal_noindex(df, expected)
+
+
+def test_filter_by_date_stop_only(sample_data):
+    """It should keep rows before a specific date"""
+    df = pd.DataFrame(sample_data)
+    df = filter_by_date(df, 'date', stop='2018-01-30')
+    expected = pd.DataFrame([
+        {'date': '2018-01-28', 'value': 0},
+        {'date': '2018-01-28', 'value': 1},
+        {'date': '2018-01-29', 'value': 2},
+    ])
+    assert_frame_equal_noindex(df, expected)
+
+
+def test_filter_by_date_stop_only_with_offset(sample_data):
+    """It should keep rows before a specific date with offset"""
+    df = pd.DataFrame(sample_data)
+    df = filter_by_date(df, 'date', stop='(2018-01-30) - 1d')
     expected = pd.DataFrame([
         {'date': '2018-01-28', 'value': 0},
         {'date': '2018-01-28', 'value': 1},
@@ -66,7 +133,7 @@ def test_filter_by_date_start_only(sample_data):
 
 
 def test_filter_by_date_range(sample_data):
-    """It should filter rows on a specific range"""
+    """It should keep rows on a specific range"""
     df = pd.DataFrame(sample_data)
     df = filter_by_date(df, 'date', start='2018-01-28', stop='2018-02-02')
     expected = pd.DataFrame([
@@ -80,26 +147,13 @@ def test_filter_by_date_range(sample_data):
     assert_frame_equal_noindex(df, expected)
 
 
-def test_filter_by_date_start_delta(sample_data):
-    """It should keep only rows with start <= date < start+delta"""
+def test_filter_by_date_range_with_offsets(sample_data):
+    """It should keep rows on a specific range with offsets"""
     df = pd.DataFrame(sample_data)
-    df = filter_by_date(df, 'date', start='2018-01-28', delta='3d')
+    df = filter_by_date(df, 'date', start='(2018-01-28)+1d', stop='(2018-02-02)-2d')
     expected = pd.DataFrame([
-        {'date': '2018-01-28', 'value': 0},
-        {'date': '2018-01-28', 'value': 1},
         {'date': '2018-01-29', 'value': 2},
         {'date': '2018-01-30', 'value': 3},
-    ])
-    assert_frame_equal_noindex(df, expected)
-
-
-def test_filter_by_date_stop_delta(sample_data):
-    """It should keep only rows with stop-delta <= date < stop"""
-    df = pd.DataFrame(sample_data)
-    df = filter_by_date(df, 'date', stop='2018-02-01', delta='2d')
-    expected = pd.DataFrame([
-        {'date': '2018-01-30', 'value': 3},
-        {'date': '2018-01-31', 'value': 4},
     ])
     assert_frame_equal_noindex(df, expected)
 
@@ -107,9 +161,19 @@ def test_filter_by_date_stop_delta(sample_data):
 def test_filter_by_date_start_symbolic_today(sample_data):
     """It should understand 'TODAY' as a valid start date"""
     df = pd.DataFrame(sample_data)
-    df = filter_by_date(df, 'date', start='TODAY')
+    df = filter_by_date(df, 'date', atdate='TODAY')
     expected = pd.DataFrame([
         {'date': TODAY, 'value': 8},
+    ])
+    assert_frame_equal_noindex(df, expected)
+
+
+def test_filter_by_date_start_symbolic_today_and_offset(sample_data):
+    """It should understand 'TODAY' with offset as a valid start date"""
+    df = pd.DataFrame(sample_data)
+    df = filter_by_date(df, 'date', start='(TODAY) + 1d')
+    expected = pd.DataFrame([
+        {'date': TOMORROW, 'value': 9},
     ])
     assert_frame_equal_noindex(df, expected)
 
@@ -117,7 +181,7 @@ def test_filter_by_date_start_symbolic_today(sample_data):
 def test_filter_by_date_start_symbolic_yesterday(sample_data):
     """It should understand 'YESTERDAY' as a valid start date"""
     df = pd.DataFrame(sample_data)
-    df = filter_by_date(df, 'date', start='YESTERDAY')
+    df = filter_by_date(df, 'date', atdate='YESTERDAY')
     expected = pd.DataFrame([
         {'date': YESTERDAY, 'value': 7},
     ])
@@ -127,39 +191,9 @@ def test_filter_by_date_start_symbolic_yesterday(sample_data):
 def test_filter_by_date_start_symbolic_tomorrow(sample_data):
     """It should understand 'TOMORROW' as a valid start date"""
     df = pd.DataFrame(sample_data)
-    df = filter_by_date(df, 'date', start='TOMORROW')
+    df = filter_by_date(df, 'date', atdate='TOMORROW')
     expected = pd.DataFrame([
         {'date': TOMORROW, 'value': 9},
-    ])
-    assert_frame_equal_noindex(df, expected)
-
-
-def test_filter_by_date_stop_symbolic_today(sample_data):
-    """It should understand 'TODAY' as a valid stop date"""
-    df = pd.DataFrame(sample_data)
-    df = filter_by_date(df, 'date', stop='TODAY', delta='1d')
-    expected = pd.DataFrame([
-        {'date': YESTERDAY, 'value': 7},
-    ])
-    assert_frame_equal_noindex(df, expected)
-
-
-def test_filter_by_date_stop_symbolic_yesterday(sample_data):
-    """It should understand 'YESTERDAY' as a valid stop date"""
-    df = pd.DataFrame(sample_data)
-    df = filter_by_date(df, 'date', stop='YESTERDAY', delta='1d')
-    expected = pd.DataFrame([
-        {'date': BEFORE_YESTERDAY, 'value': 10},
-    ])
-    assert_frame_equal_noindex(df, expected)
-
-
-def test_filter_by_date_stop_symbolic_tomorrow(sample_data):
-    """It should understand 'TOMORROW' as a valid stop date"""
-    df = pd.DataFrame(sample_data)
-    df = filter_by_date(df, 'date', stop='TOMORROW', delta='1d')
-    expected = pd.DataFrame([
-        {'date': TODAY, 'value': 8},
     ])
     assert_frame_equal_noindex(df, expected)
 
@@ -171,9 +205,25 @@ def test_date_fmt():
         {'date': '03 2018', 'value': 2},
         {'date': '03 2018', 'value': 3}
     ])
-    df = filter_by_date(df, date_col='date', date_fmt='%m %Y', start='02 2018', delta='90d')
+    df = filter_by_date(df, date_col='date', date_fmt='%m %Y', start='(02 2018)+10d')
     expected = pd.DataFrame([
         {'date': '03 2018', 'value': 2},
         {'date': '03 2018', 'value': 3}
     ])
     assert_frame_equal_noindex(df, expected)
+
+
+def test_parse_date():
+    """It should be able to parse dates."""
+    assert parse_date('2018-01-02', '%Y-%m-%d') == date(2018, 1, 2)
+    assert parse_date('2018 01', '%Y %m') == date(2018, 1, 1)
+    assert parse_date('(2018-01-02)', '%Y-%m-%d') == date(2018, 1, 2)
+    assert parse_date('(2018-01-02)+1d', '%Y-%m-%d') == date(2018, 1, 3)
+    assert parse_date('(2018-01-02) + 1d', '%Y-%m-%d') == date(2018, 1, 3)
+    assert parse_date('(2018-01-02)-1d', '%Y-%m-%d') == date(2018, 1, 1)
+    assert parse_date('(2018-01-02) - 1d', '%Y-%m-%d') == date(2018, 1, 1)
+    assert parse_date('TODAY', '%Y-%m-%d') == date.today()
+    yesterday = date.today() - timedelta(days=1)
+    tomorrow = date.today() + timedelta(days=1)
+    assert parse_date('(TODAY) + 1day', '%Y-%m-%d') == tomorrow
+    assert parse_date('(TODAY) - 1day', '%Y-%m-%d') == yesterday
--- toucan_data_sdk/utils/postprocess/filter_by_date.py
+++ toucan_data_sdk/utils/postprocess/filter_by_date.py
@@ -1,20 +1,22 @@
 """date filtering helpers."""
 
-from uuid import uuid4
 from datetime import date, datetime
+import re
+from uuid import uuid4
 
 import pandas as pd
 
 
-def _norm_date(datestr: str, date_fmt: str) -> datetime:
+def _norm_date(datestr: str, date_fmt: str) -> date:
     """normalize symbolic date values (e.g. 'TODAY')
 
     Convert a symbolic value in a valid date.
     Currenlty known symbolic values are 'TODAY', 'YESTERDAY' and 'TOMORROW'.
 
+    NOTE: This function will return `date` (not `datetime`) instances.
 
     Parameters:
-        `datestr`: the date to parse
+        `datestr`: the date to parse, formatted as `date_fmt`
         `date_fmt`: expected output date format
 
     Returns:
@@ -26,26 +28,75 @@ def _norm_date(datestr: str, date_fmt: str) -> datetime:
         days = {'TODAY': 0, 'YESTERDAY': -1, 'TOMORROW': 1}[datestr.upper()]
         return date.today() + pd.Timedelta(days=days)
     except KeyError:
-        return datetime.strptime(datestr, date_fmt)
+        return datetime.strptime(datestr, date_fmt).date()
 
 
-def filter_by_date(df, date_col, date_fmt='%Y-%m-%d', start=None, stop=None, delta=None):
+def parse_date(datestr: str, date_fmt: str) -> date:
+    """parse `datestr` and return corresponding date object.
+
+    `datestr` should be a string matching `date_fmt` and parseable by
+    `strptime` but some offset can also be added using `(datestr) + OFFSET` or
+    `(datestr) - OFFSET` syntax. When using this syntax, `OFFSET` should be
+    understable by `pandas.Timedelta` (cf.
+    http://pandas.pydata.org/pandas-docs/stable/timedeltas.html) and `datestr`
+    MUST be wrapped with parenthesis.
+
+    Additionally, the following symbolic names are supported:
+    `TODAY`, `YESTERDAY`, `TOMORROW`.
+
+    Example usage:
+
+    >>> parse_date('2018-01-01', '%Y-%m-%d')
+    datetime.datetime(2018, 1, 1, 0, 0)
+    >>> parse_date('(2018-01-01) + 1day', '%Y-%m-%d')
+    datetime.datetime(2018, 1, 2, 0, 0)
+
+    Parameters:
+        `datestr`: the date to parse, formatted as `date_fmt`
+        `date_fmt`: expected date format
+
+    Returns:
+        The `date` object. If date could not be parsed, a ValueError
+        will be raised.
+    """
+    rgx = re.compile(r'\((?P<date>.*)\)(\s*(?P<sign>[+-])(?P<offset>.*))?$')
+    datestr = datestr.strip()
+    match = rgx.match(datestr)
+    # if regexp doesn't match, date must match the expected format
+    if match is None:
+        return _norm_date(datestr, date_fmt)
+    datestr = match.group('date').strip()
+    dateobj = _norm_date(datestr, date_fmt)
+    offset = match.group('offset')
+    if offset:
+        sign = match.group('sign')
+        delta = pd.Timedelta(offset)
+        dateobj = (dateobj + delta) if sign == '+' else (dateobj - delta)
+    return dateobj
+
+
+def filter_by_date(df, date_col, date_fmt='%Y-%m-%d', start=None, stop=None, atdate=None):
     """filter dataframe `df` by date.
 
-    This function will interpret `start`, `stop` and `delta` and build
+    This function will interpret `start`, `stop` and `atdate` and build
     the corresponding date range. The caller must specify either:
 
-    - `start`: keep all rows matching this date exactly,
+    - `atdate`: keep all rows matching this date exactly,
+    - `start`: keep all rows matching this date onwards.
+    - `stop`: keep all rows matching dates before this one.
     - `start` and `stop`: keep all rows between `start` and `stop`,
-    - `start` and `delta`: keep all rows between `start` and `start` + `delta`,
-    - `stop` and `delta`: keep all rows between `stop` - `delta` and `start`.
 
     Any other combination will raise an error. The lower bound of the date range
     will be included, the upper bound will be excluded.
 
-    When specified, `start` and `stop` values are expected to match the
-    `date_fmt` format or to be a known symbolic value (i.e. 'TODAY',
-    'YESTERDAY' or 'TOMORROW').
+    When specified, `start`, `stop` and `atdate` values are expected to match the
+    `date_fmt` format or a known symbolic value (i.e. 'TODAY', 'YESTERDAY' or
+    'TOMORROW').
+
+    Additionally, the offset syntax "(date) + offset" is also supported (Mind
+    the parenthesis around the date string). In that case, the offset must be
+    one of the syntax supported by `pandas.Timedelta` (cf.
+    http://pandas.pydata.org/pandas-docs/stable/timedeltas.html)
 
     Parameters:
         `df`: the dataframe to filter
@@ -53,38 +104,31 @@ def filter_by_date(df, date_col, date_fmt='%Y-%m-%d', start=None, stop=None, del
         `date_fmt`: expected date format in column `date_col`
         `start`: if specified, lower bound (included) of the date range
         `stop`: if specified, upper bound (excluded) of the date range
-        `delta`: if specified, date range span. The value must be a string
-            understable by `pandas.Timedelta` (cf.
-            http://pandas.pydata.org/pandas-docs/stable/timedeltas.html)
+        `atdate`: if specified, the exact date we're filtering on
 
     Returns:
         The filtered dataframe
     """
     mask = None
-    if start is None and stop is None:
-        raise TypeError('either "start" or "stop" must be specified')
+    if start is None and stop is None and atdate is None:
+        raise TypeError('either "start", "stop" or "atdate" must be specified')
+    if start is not None and atdate is not None:
+        raise TypeError('"start" and "atdate" are mutually exclusive')
+    if stop is not None and atdate is not None:
+        raise TypeError('"stop" and "atdate" are mutually exclusive')
     # add a new column that will hold actual date objects instead of strings.
     # This column is just temporary and will be removed before returning the
     # filtered dataframe.
     filtercol = str(uuid4())
     df[filtercol] = pd.to_datetime(df[date_col], format=date_fmt)
-    if start is not None and stop is not None:
-        if delta is not None:
-            raise TypeError('if "start" and "stop" are specified, "delta" must be None')
-        mask = ((df[filtercol] >= _norm_date(start, date_fmt)) &
-                (df[filtercol] < _norm_date(stop, date_fmt)))
+    if atdate is not None:
+        print('atdate', parse_date(atdate, date_fmt))
+        mask = df[filtercol] == parse_date(atdate, date_fmt)
+    elif start is not None and stop is not None:
+        mask = ((df[filtercol] >= parse_date(start, date_fmt)) &
+                (df[filtercol] < parse_date(stop, date_fmt)))
     elif stop is None:
-        start = _norm_date(start, date_fmt)
-        if delta is None:
-            mask = df[filtercol] == start
-        else:
-            stop = start + pd.Timedelta(delta)
-            mask = (df[filtercol] >= start) & (df[filtercol] < stop)
+        mask = df[filtercol] >= parse_date(start, date_fmt)
     elif start is None:
-        if delta is None:
-            raise TypeError(
-                '"stop" without "start" nor "delta" is forbidden, use "start" instead')
-        stop = _norm_date(stop, date_fmt)
-        start = stop - pd.Timedelta(delta)
-        mask = (df[filtercol] >= start) & (df[filtercol] < stop)
+        mask = df[filtercol] < parse_date(stop, date_fmt)
     return df.loc[mask].drop(filtercol, axis=1)
