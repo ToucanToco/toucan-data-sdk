86ba3b1eb24819e4afe4e0b4135deb1cf287e211
raphael <raphael.huille@toucantoco.com>
2019-03-08 12:11:31 +0100

    converter docstring update

--- toucan_data_sdk/utils/postprocess/converter.py
+++ toucan_data_sdk/utils/postprocess/converter.py
@@ -1,43 +1,46 @@
 import pandas as pd
 
 
-def convert_str_to_datetime(df, *, column=None, format=None):
+def convert_str_to_datetime(df, *, column=None:str, format=None:str):
     """
     Convert string column into datetime column
-    :param df: Dataframe
-    :param column: name of the column to format
-    :param format: format of the values
-    :return: df
+
+    - column: name of the column to format
+    - format: current format of the values
+    List of available format:
+    https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
     """
     df[column] = pd.to_datetime(df[column], format=format)
     return df
 
 
-def convert_datetime_to_str(df, *, column=None, format=None, new_column=None):
+def convert_datetime_to_str(df, *, column=None:str, format=None:str, new_column=None:str):
     """
     Convert datetime column into string column
-    :param df: Dataframe
-    :param column: name of the column to format
-    :param format: format of the result values
-    :param new_column: name of the output
-    :return: df
+
+    - column: name of the column to format
+    - new_column: name of the output
+    - format: format of the result values
+    List of available format:
+    https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
     """
     new_column = new_column or column
     df[new_column] = df[column].dt.strftime(format)
     return df
 
 
-def change_date_format(df, *, column, output_format, input_format=None, new_column=None,
-                       new_time_zone=None):
+def change_date_format(df, *, column:str, output_format:str, input_format=None:str, new_column=None:str,
+                       new_time_zone=None:str):
     """
-    Convert datetime column into string column
-    :param df: Dataframe
-    :param column: name of the column to format
-    :param output_format: format of the output values
-    :param input_format: format of the input values (If None, let the parser detect it)
-    :param new_column: name of the output column
-    :param new_time_zone: name of new time zone (If None, no time zone conversion is done)
-    :return: df
+    Convert a date column from a format (input_format) to an other (output_format)
+
+    - column: name of the column to change the format
+    - output_format: format of the output values
+    - input_format (optional): format of the input values - if 'None' let the parser detect it
+    - new_column (optional): name of the output column - if 'None' overwrite column
+    - new_time_zone (optional): name of new time zone - if 'None', no time zone conversion is done
+    List of available format:
+    https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
     """
     new_column = new_column or column
     df[new_column] = (pd.to_datetime(df[column], format=input_format, utc=True)
@@ -46,13 +49,13 @@ def change_date_format(df, *, column, output_format, input_format=None, new_colu
     return df
 
 
-def cast(df, column, type, new_column=None):
+def cast(df, column:str, type:str, new_column=None:str):
     """
     Convert column's type into type
-    :param df: Dataframe
-    :param column: name of the column to format
-    :param type: desired type of the column
-    :return: df
+    - column: name of the column to format
+    - type: desired type of the column
+    - new_column (optional): name of the output column - if 'None' overwrite column
+    Available type : 'str' (from string), 'int' (for integer), 'float' (for real number)
     """
     new_column = new_column or column
     df[new_column] = df[column].astype(type)
