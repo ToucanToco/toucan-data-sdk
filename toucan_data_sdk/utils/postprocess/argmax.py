10161b9e94b669beac726b63b141a47fec876add
raphael <raphael.huille@toucantoco.com>
2019-03-08 11:42:12 +0100

    argmin and argmax docstring update

--- toucan_data_sdk/utils/postprocess/argmax.py
+++ toucan_data_sdk/utils/postprocess/argmax.py
@@ -1,8 +1,18 @@
-def argmax(df, column):
+def argmax(df, column:str):
+    """
+    Keep the row of the data corresponding to the maximal value in a column
+
+    - column: name of the column containing the value you want to keep the maximum
+    """
     df = df[df[column] == df[column].max()]
     return df
 
 
-def argmin(df, column):
+def argmin(df, column:str):
+    """
+    Keep the row of the data corresponding to the minimal value in a column
+
+    - column: name of the column containing the value you want to keep the minimum
+    """
     df = df[df[column] == df[column].min()]
     return df
