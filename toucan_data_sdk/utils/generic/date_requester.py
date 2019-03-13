083e8d8658f58b6188407b1e09d7f7efff27463d
PrettyWood <em.jolibois@gmail.com>
2019-03-13 12:12:07 +0100

    fix flake8 and remove bold for description

--- toucan_data_sdk/utils/generic/date_requester.py
+++ toucan_data_sdk/utils/generic/date_requester.py
@@ -2,15 +2,22 @@ import pandas as pd
 from typing import Dict
 
 
-def date_requester_generator(df, date_column: str, frequency: str, date_column_format: str = None,
-                             format: str = '%Y-%m-%d', granularities: Dict[str, str] = None,
-                             others_format: Dict[str, str] = None, times_delta: Dict[str, str] = None):
+def date_requester_generator(
+        df: pd.DataFrame,
+        date_column: str,
+        frequency: str,
+        date_column_format: str = None,
+        format: str = '%Y-%m-%d',
+        granularities: Dict[str, str] = None,
+        others_format: Dict[str, str] = None,
+        times_delta: Dict[str, str] = None
+) -> pd.DataFrame:
     """
-    **From a dataset containing dates in a column, return a dataset**
-    **with at least 3 columns :**
-    **- "DATE" : Label of date**
-    **- "DATETIME" : Date in datetime dtype**
-    **- "GRANULARITY" : Granualrity of date**
+    From a dataset containing dates in a column, return a dataset
+    with at least 3 columns :
+    - "DATE" : Label of date
+    - "DATETIME" : Date in datetime dtype
+    - "GRANULARITY" : Granualrity of date
 
     - `date_column` (str): name of column containing the date in the dataframe
     - `frequency` (str) : see [pandas doc](
@@ -32,7 +39,6 @@ def date_requester_generator(df, date_column: str, frequency: str, date_column_f
             NB : same format in Toucan Toco
             _Example : '%d/%m/%Y'_ (see [pandas doc](
             https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior))
-
     - `times_delta` (optional: dict) : Add new columns for each key
         - key (str) : name of the column
         - values (str): time delta
