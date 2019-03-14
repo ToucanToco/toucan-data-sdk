1f755d9518e79af8795aa006746199e1746ce17d
PrettyWood <em.jolibois@gmail.com>
2019-03-14 18:47:18 +0100

    sort

--- toucan_data_sdk/utils/postprocess/sort.py
+++ toucan_data_sdk/utils/postprocess/sort.py
@@ -3,10 +3,46 @@ from typing import List
 
 def sort(df, columns: List[str], order='asc'):
     """
-    Sort data
+
+    Sort the data by the value in specified columns
+
     ---
-    - `columns` (list): list of the names of the columns to sort
-    - `order` (optional: str): 'asc' (default) or 'desc'
+
+    ### Parameters
+
+    *mandatory :*
+    - `columns` (*list*): list of the names of the columns to sort
+
+    *optional :*
+    - `order` (*str*): 'asc' (default) or 'desc'
+
+    ---
+
+    ### Example
+
+    **Input**
+
+    | variable | value |
+    |:--------:|:-----:|
+    |     A    |  220  |
+    |     B    |  200  |
+    |     C    |  300  |
+    |     D    |  100  |
+
+    ```cson
+    sort:
+      columns: ['variable']
+      order: 'asc'
+    ```
+
+    **Output**
+
+    | variable | value |
+    |:--------:|:-----:|
+    |     D    |  100  |
+    |     B    |  200  |
+    |     A    |  220  |
+    |     C    |  300  |
     """
     ascending = order != 'desc'
     return df.sort_values(columns, ascending=ascending)
