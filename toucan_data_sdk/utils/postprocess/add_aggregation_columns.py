affc6f47fddcc926aeeffb9def23e91816aa1c5c
PrettyWood <em.jolibois@gmail.com>
2019-03-14 13:40:47 +0100

    update add_aggregation_columns

--- toucan_data_sdk/utils/postprocess/add_aggregation_columns.py
+++ toucan_data_sdk/utils/postprocess/add_aggregation_columns.py
@@ -11,44 +11,59 @@ def add_aggregation_columns(
     """
     Add new columns containing aggregations values on existing columns
 
-    - group_cols: columns used to aggregate the data
-    - aggregations: keys are name of new columns and values are aggregation functions
-        Examples of aggregation functions : 'sum', 'max'
-        Available aggregation functions are listed here:
-        https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html#aggregation
+    ---
 
+    ### Parameters
 
-    # Examples #
+    - `group_cols` (str or list): columns used to aggregate the data
+    - `aggregations` (dict): keys are name of new columns and values are aggregation functions
+       Examples of aggregation functions : 'sum', 'max'
+       Available aggregation functions are listed [here](
+       https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html#aggregation)
 
-    df =
-       year  group libelle      mois  value  total
-    0  2018  e-EKO       a  20180101      3      3
-    1  2018  e-EKO       b  20180101      0      3
-    2  2018  e-EKO       c  20180101      4      7
-    3  2018    DOQ       q  20180101     33     33
-    4  2018    DOQ       b  20180101     54     87
-    5  2018     DO       q  20180101     13     13
-    6  2018     DO       c  20180101     14     27
+    ---
 
-    df = groupby_append(
-        df,
-        ['mois', 'group'],
-        {
-            'max_total': {'total': 'max'},
-            'sum_total': {'total': 'sum'},
-            'mean_value': {'value': 'mean'}
-        }
-    )
+    ### Example
 
-    df =
-       year  group libelle      mois  value  total  max_total  sum_total  mean_value
-    0  2018  e-EKO       a  20180101      3      3          7         13    2.333333
-    1  2018  e-EKO       b  20180101      0      3          7         13    2.333333
-    2  2018  e-EKO       c  20180101      4      7          7         13    2.333333
-    3  2018    DOQ       q  20180101     33     33         87        120   43.500000
-    4  2018    DOQ       b  20180101     54     87         87        120   43.500000
-    5  2018     DO       q  20180101     13     13         27         40   13.500000
-    6  2018     DO       c  20180101     14     27         27         40   13.500000
+    **Input**
+
+    | ENTITY | YEAR | VALUE_1 | VALUE_2 |
+    |:------:|:----:|:-------:|:-------:|
+    |    A   | 2017 |    10   |    3    |
+    |    A   | 2017 |    20   |    1    |
+    |    A   | 2018 |    10   |    5    |
+    |    A   | 2018 |    30   |    4    |
+    |    B   | 2017 |    60   |    4    |
+    |    B   | 2017 |    40   |    3    |
+    |    B   | 2018 |    50   |    7    |
+    |    B   | 2018 |    60   |    6    |
+
+    ```cson
+    add_aggregation_columns:
+      group_cols: ['ENTITY', 'YEAR']
+      aggregations:
+        sum_value1:
+          VALUE_1: 'sum'  # sum of `VALUE_1` put in `sum_value1` column
+        max_value1:
+          VALUE_1: 'max'  # max of `VALUE_1` put in `max_value1` column
+        mean_value2:
+          VALUE_2: 'mean'  # mean of `VALUE_2` put in `mean_value2` column
+    ]
+    ```
+
+    **Output**
+
+    | ENTITY | YEAR | VALUE_1 | VALUE_2 | sum_value1 | max_value1 | mean_value2 |
+    |:------:|:----:|:-------:|:-------:|:----------:|:----------:|:-----------:|
+    |    A   | 2017 |    10   |    3    |     30     |     20     |     2.0     |
+    |    A   | 2017 |    20   |    1    |     30     |     20     |     2.0     |
+    |    A   | 2018 |    10   |    5    |     40     |     30     |     4.5     |
+    |    A   | 2018 |    30   |    4    |     40     |     30     |     4.5     |
+    |    B   | 2017 |    60   |    4    |    100     |     60     |     3.5     |
+    |    B   | 2017 |    40   |    3    |    100     |     60     |     3.5     |
+    |    B   | 2018 |    50   |    7    |    110     |     60     |     6.5     |
+    |    B   | 2018 |    60   |    6    |    110     |     60     |     6.5     |
+
     """
     group = df.groupby(group_cols)
     for new_col, aggs in aggregations.items():
