6619f57bee29708a45e516a129510464cddbd5d9
PrettyWood <em.jolibois@gmail.com>
2019-03-14 20:02:54 +0100

    two_values_melt

--- toucan_data_sdk/utils/generic/two_values_melt.py
+++ toucan_data_sdk/utils/generic/two_values_melt.py
@@ -6,34 +6,51 @@ def two_values_melt(
     df,
     first_value_vars: List[str],
     second_value_vars: List[str],
-    var_name: List[str],
+    var_name: str,
     value_name: str
 ):
     """
-    First, build two DataFrames from the original one: one to compute a melt
-    for the value, another one to compute a melt for the evolution. Second,
-    merge these two DataFrames.
+    Transforms one or multiple columns into rows.
+    Unlike melt function, two value columns can be returned by
+    the function (e.g. an evolution column and a price column)
+
     ---
-    - `first_value_vars`: value_vars of a pandas melt, for the first
-        value columns of the DataFrame
-    - `second_value_vars`: value_vars of a pandas melt, for the second
-        value columns of the DataFrame
-    - `var_name`: var_names of a pandas melt
-    - `value_name`: value_name of a pandas melt
+
+    ### Parameters
+
+    *mandatory :*
+    - `first_value_vars` (*list of str*): name of the columns corresponding to the first returned value column
+    - `second_value_vars` (*list of str*): name of the columns corresponding to the second returned value column
+    - `var_name` (*str*): name of the column containing values in first_value_vars
+    - `value_name` (*str*): suffix of the two value columns (suffix_first / suffix_second)
     ---
-    **Examples**
 
-    input :
+    ### Example
 
-    | ... | <some1> | <some2> | <some1_evol> | <some2_evol> |
-    | ... | <val1>  | <val2>  | <evol1>      | <evol2>      |
+    **Input**
 
-    output :
+    |    Region |      avg |       total |  evo_avg |   evo_total |
+    |:---------:|:--------:|:-----------:|:--------:|:-----------:|
+    |         A |        50|         100 |        1 |           4 |
+    |         B |       40 |         250 |        2 |           5 |
 
-    | ... | variable  | value  | evolution
-    | ... | --------- | ------ | ---------
-    | ... |  <some1>  | <val1> | <evol1>
-    | ... |  <some2>  | <val2> | <evol2>
+
+    ```cson
+    two_values_melt:
+        first_value_vars: ["avg", "total"]
+        second_value_vars: ["evo_avg", "evo_total"]
+        var_name: "type"
+        value_name: "value"
+    ```
+
+    **Output**
+
+    |    Region |     type |  value_first |  value_second |
+    |:---------:|:--------:|:------------:|:-------------:|
+    |         A |       avg|           50 |             1 |
+    |         A |     total|          100 |             4 |
+    |         B |       avg|           40 |             2 |
+    |         B |       avg|          250 |             5 |
     """
     value_name_first = value_name + '_first'
     value_name_second = value_name + '_second'
