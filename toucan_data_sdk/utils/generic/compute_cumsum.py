b6acfeae772085aae973c625bf1bea6bef97c1a0
PrettyWood <em.jolibois@gmail.com>
2019-03-14 14:37:47 +0100

    update compute_cumsum

--- toucan_data_sdk/utils/generic/compute_cumsum.py
+++ toucan_data_sdk/utils/generic/compute_cumsum.py
@@ -1,9 +1,9 @@
+from typing import List
+
 from toucan_data_sdk.utils.helpers import (
     check_params_columns_duplicate,
     ParamsValueError
 )
-
-from typing import List
 
 
 def compute_cumsum(
@@ -11,41 +11,52 @@ def compute_cumsum(
     id_cols: List[str],
     reference_cols: List[str],
     value_cols: List[str],
-    new_value_cols=None,
-    cols_to_keep=None
+    new_value_cols: List[str] = None,
+    cols_to_keep: List[str] = None
 ):
     """
     Compute cumsum for a group of columns.
 
-    - `id_cols` are the columns id to create each group,
-    - `reference_cols` are the columns to order the cumsum,
-    - `value_cols` are the columns to cumsum,
-    - `new_value_cols` are the new columns with the result cumsum
-    - `cols_to_keep` are other column to keep in the dataframe. This option can
-     be used if there is only one row by group [id_cols + reference_cols]
+    ---
 
+    ### Parameters
 
-    # Example #
+    - `id_cols` (list): the columns id to create each group
+    - `reference_cols` (list): the columns to order the cumsum
+    - `value_cols` (list): the columns to cumsum
+    - `new_value_cols` (list): the new columns with the result cumsum
+    - `cols_to_keep` (list): other columns to keep in the dataset.
+      This option can be used if there is only one row by group [id_cols + reference_cols]
 
-    MONTH  DAY NAME  VALUE  X
-     1      1    A      1  lo
-     2      1    A      1  lo
-     2     15    A      1  la
-     1     15    B      1  la
+    ---
 
-    The function `compute_cumsum` with the arguments :
-            id_cols=['NAME']
-            reference_cols=['MONTH','DAY']
-            cumsum_cols=['VALUE']
-            cols_to_keep=['X']
-    give as a result :
+    ### Example
 
+    **Input**
 
-    NAME  MONTH  DAY  X  VALUE
-     A     1      1  lo      1
-     A     2      1  la      2
-     A     2     15  lo      3
-     B     1     15  la      1
+    MONTH | DAY | NAME | VALUE | X
+    :---:|:---:|:--:|:---:|:---:
+     1   |   1 |   A  |  1 | lo
+     2   |   1 |   A  |  1 | lo
+     2   |  15 |   A  |  1 | la
+     1   |  15 |   B  |  1 | la
+
+    ```cson
+    compute_cumsum:
+        id_cols: ['NAME']
+        reference_cols: ['MONTH', 'DAY']
+        cumsum_cols: ['VALUE']
+        cols_to_keep: ['X']
+    ```
+
+    **Output**
+
+    NAME | MONTH | DAY | X | VALUE
+    :---:|:---:|:--:|:---:|:---:
+     A  |   1  |    1 | lo  |    1
+     A  |   2  |    1 | la  |    2
+     A  |   2  |   15 | lo  |    3
+     B  |   1  |   15 | la  |    1
     """
     if cols_to_keep is None:
         cols_to_keep = []
