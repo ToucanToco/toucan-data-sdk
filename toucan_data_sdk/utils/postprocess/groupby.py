e63c6eee1097a6b0390ce3f39e8924a11db02e4c
vincent_d <vincent.destrait@toucantoco.com>
2019-03-26 10:14:20 +0100

    transform loop to list comprehension

--- toucan_data_sdk/utils/postprocess/groupby.py
+++ toucan_data_sdk/utils/postprocess/groupby.py
@@ -59,11 +59,7 @@ def groupby(df, *, group_cols: Union[str, List[str]],
     if df.columns.nlevels == 2:
         level_0 = df.columns.get_level_values(0)
         level_1 = df.columns.get_level_values(1)
-        new_columns = []
-        for (x, y) in zip(level_1, level_0):
-            if x == '':
-                new_columns.append(y)
-            else:
-                new_columns.append(f'{x}_{y}')
+        new_columns = [(f'{x}_{y}' if x else y) for (x, y)
+                       in zip(level_1, level_0)]
         df.columns = new_columns
     return df
