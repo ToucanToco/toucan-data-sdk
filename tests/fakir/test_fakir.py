a0f7ddb44ea922f6b571e565377539c06c76097e
PrettyWood <em.jolibois@gmail.com>
2019-03-27 17:56:21 +0100

    make code a bit more explicit

--- tests/fakir/test_fakir.py
+++ tests/fakir/test_fakir.py
@@ -2,26 +2,26 @@ from toucan_data_sdk.fakir.fake_data_generator import fake_data_generator
 
 
 conf_1 = [
-  {"type": "label", "values": ['Paris', 'Marseille', 'Lyons'], "name": "Cities"},
-  {"type": "label", "values": ['A', 'B', 'C'], "name": "Agence_type"},
-  {"type": "number", "min": 0, "max": 10, "digits": 2, "name": "CA"}
+  {'type': 'label', 'values': ['Paris', 'Marseille', 'Lyons'], 'name': 'Cities'},
+  {'type': 'label', 'values': ['A', 'B', 'C'], 'name': 'Agence_type'},
+  {'type': 'number', 'min': 0, 'max': 10, 'digits': 2, 'name': 'CA'}
 ]
 
 conf_2 = conf_1 + [
-  {"type": "number", "min": 10, "max": 100, "digits": 4, "name": "Percentage"},
-  {"type": "label", "values": ["Man", "Women"], "name": "Sexe"}
+  {'type': 'number', 'min': 10, 'max': 100, 'digits': 4, 'name': 'Percentage'},
+  {'type': 'label', 'values': ['Man', 'Women'], 'name': 'Sexe'}
 ]
 
 
-def test_fakir():
+def test_fake_data_generator():
     fake_data = fake_data_generator(conf_1)
-    assert set(fake_data.columns) == {"Cities", "Agence_type", "CA"}
-    assert len(fake_data) == 3*3
-    assert (fake_data['CA'] > 0).all() & (fake_data['CA'] < 10).all()
-    assert (fake_data.round(decimals=2)['CA'] == fake_data['CA']).all()
-    assert (fake_data['Cities'] == 'Paris').sum() == 3
-    assert (fake_data['Agence_type'] == 'A').sum() == 3
+    assert set(fake_data.columns) == {'Cities', 'Agence_type', 'CA'}
+    assert fake_data.shape == (3 * 3, 3)
+    assert fake_data['CA'].between(0, 10).all()
+    assert fake_data['CA'].round(2).equals(fake_data['CA'])
+    assert fake_data[fake_data['Cities'] == 'Paris'].shape[0] == 3
+    assert fake_data[fake_data['Agence_type'] == 'A'].shape[0] == 3
 
     fake_data = fake_data_generator(conf_2)
-    assert set(fake_data.columns) == {"Cities", "Agence_type", "CA", 'Sexe', 'Percentage'}
-    assert len(fake_data) == 3*3*2
+    assert set(fake_data.columns) == {'Cities', 'Agence_type', 'CA', 'Sexe', 'Percentage'}
+    assert fake_data.shape == (3 * 3 * 2, 5)
