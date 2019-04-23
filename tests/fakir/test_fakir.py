c3ca874e1b64f4bdcc2edda750a72d45d1561d8a
Raphael Huille <raphael.huille@gmail.com>
2019-04-23 16:48:20 +0200

    f/enhance fakir (#16)
    * set a default valus for  params of conf at 4
    
    * add test on digit

--- tests/fakir/test_fakir.py
+++ tests/fakir/test_fakir.py
@@ -4,11 +4,11 @@ from toucan_data_sdk.fakir.fake_data_generator import fake_data_generator
 conf_1 = [
   {'type': 'label', 'values': ['Paris', 'Marseille', 'Lyons'], 'name': 'Cities'},
   {'type': 'label', 'values': ['A', 'B', 'C'], 'name': 'Agence_type'},
-  {'type': 'number', 'min': 0, 'max': 10, 'digits': 2, 'name': 'CA'}
+  {'type': 'number', 'min': 0, 'max': 10, 'name': 'CA'}
 ]
 
 conf_2 = conf_1 + [
-  {'type': 'number', 'min': 10, 'max': 100, 'digits': 4, 'name': 'Percentage'},
+  {'type': 'number', 'min': 10, 'max': 100, 'digits': 2, 'name': 'Percentage'},
   {'type': 'label', 'values': ['Man', 'Women'], 'name': 'Sexe'}
 ]
 
@@ -18,10 +18,11 @@ def test_fake_data_generator():
     assert set(fake_data.columns) == {'Cities', 'Agence_type', 'CA'}
     assert fake_data.shape == (3 * 3, 3)
     assert fake_data['CA'].between(0, 10).all()
-    assert fake_data['CA'].round(2).equals(fake_data['CA'])
+    assert fake_data['CA'].round(4).equals(fake_data['CA'])
     assert fake_data[fake_data['Cities'] == 'Paris'].shape[0] == 3
     assert fake_data[fake_data['Agence_type'] == 'A'].shape[0] == 3
 
     fake_data = fake_data_generator(conf_2)
     assert set(fake_data.columns) == {'Cities', 'Agence_type', 'CA', 'Sexe', 'Percentage'}
+    assert fake_data['Percentage'].round(2).equals(fake_data['Percentage'])
     assert fake_data.shape == (3 * 3 * 2, 5)
